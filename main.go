9da052a92dc1610e67bb80c9d7cd36e4fe6f4304
Cesar Gimenes <crg@crg.eti.br>
2019-12-20 19:06:08 -0300

    format output

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"go/format"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 
 	"github.com/crgimenes/goconfig"
 )
@@ -12,6 +18,34 @@ type config struct {
 	OutputFile  string `cfg:"o" cfgRequired:"true" cfgHelper:"output file"`
 	PathList    string `cfg:"path" cfgRequired:"true" cfgHelper:"path list"`
 	PackageName string `cfg:"pkg" cfgHelper:"package name"`
+}
+
+var files = make(map[string]bool)
+
+func processPath(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		fmt.Printf("%q: %v\n", path, err)
+		return err
+	}
+	if info.IsDir() {
+		return nil
+	}
+	files[path] = true
+	return nil
+}
+
+func writeToFile(filename string, payload []byte) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(payload)
+	if err != nil {
+		return err
+	}
+	return file.Sync()
 }
 
 func main() {
@@ -23,20 +57,58 @@ func main() {
 		return
 	}
 
-	err = filepath.Walk(".",
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				fmt.Printf("%q: %v\n", path, err)
-				return err
-			}
-			if info.IsDir() {
-				return nil
-			}
-			fmt.Printf("> %v\n", path)
-			return nil
-		})
-	if err != nil {
-		fmt.Println(err)
-		return
+	pathList := strings.Split(cfg.PathList, ":")
+	for _, path := range pathList {
+		err = filepath.Walk(path, processPath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
+
+	var f []string
+	for k, _ := range files {
+		f = append(f, k)
+	}
+	sort.Strings(f)
+
+	var bff []byte
+	file := bytes.NewBuffer(bff)
+
+	file.WriteString("package b2go\n\n")
+
+	b := "var b = []byte{\n"
+	file.WriteString(b)
+
+	n := ""
+	for _, v := range f {
+		if n != "" {
+			file.WriteString(n)
+		}
+		n = ",\n"
+		file.WriteString("{\n")
+		data, err := ioutil.ReadFile(v)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c := ""
+		i := 0
+		for _, d := range data {
+			if c != "" {
+				file.WriteString(c)
+			}
+			if i > 7 {
+				file.WriteString("\n")
+				i = 0
+			}
+			i++
+			s := fmt.Sprintf("0x%02X", d)
+			file.WriteString(s)
+			c = ", "
+		}
+		file.WriteString(",\n}")
+	}
+	file.WriteString(",\n}")
+	out, _ := format.Source(file.Bytes())
+	writeToFile("bi2go.go", out)
 }
